abaa5e9f584adb98fa27c06a485c934d656e6489
Carlos Augusto <caarlos.san@gmail.com>
2022-11-05 12:17:12 -0300

    Resolve desafio 01

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+const fs = require('fs').promises;
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const talker = path.resolve(__dirname, './talker.json');
+
 const app = express();
+app.use(express.json());
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -12,8 +17,18 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (_req, res) => {
+  const palestrantes = await fs.readFile(talker, 'utf-8');
+  const result = palestrantes ? JSON.parse(palestrantes) : [];
+  return res.status(HTTP_OK_STATUS).json(result); 
+});
+
+// app.get('/talker/:id', async (_req, res) => {
+  
+// });  
+
 app.listen(PORT, () => {
   console.log('Online');
 });
 
-// start
+module.exports = app;
