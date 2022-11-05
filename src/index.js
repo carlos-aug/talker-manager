49fac62b24f2af509607f1a391e1a9de15568c2b
Carlos Augusto <caarlos.san@gmail.com>
2022-11-05 14:51:18 -0300

    Resolve desafio 02

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -18,14 +19,22 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const palestrantes = await fs.readFile(talker, 'utf-8');
-  const result = palestrantes ? JSON.parse(palestrantes) : [];
+  const dataTalker = await fs.readFile(talker, 'utf-8');
+  const result = dataTalker ? JSON.parse(dataTalker) : [];
   return res.status(HTTP_OK_STATUS).json(result); 
 });
 
-// app.get('/talker/:id', async (_req, res) => {
-  
-// });  
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params; 
+  const dataTalker = await fs.readFile(talker, 'utf-8');
+  const result = JSON.parse(dataTalker);
+
+  const findData = result.find((talkerId) => talkerId.id === Number(id));  
+  if (!findData) { 
+    return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  } 
+  return res.status(HTTP_OK_STATUS).json(findData); 
+  });  
 
 app.listen(PORT, () => {
   console.log('Online');
